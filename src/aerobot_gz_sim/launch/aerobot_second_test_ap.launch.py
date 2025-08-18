b0e5211d918079532962b03537059633a7147b7d
MaximVorotnikov <lada.vorotnikova68@gmail.com>
2025-08-18 17:40:27 +0300

    wip

--- src/aerobot_gz_sim/launch/aerobot_second_test_ap.launch.py
+++ src/aerobot_gz_sim/launch/aerobot_second_test_ap.launch.py
@@ -22,7 +22,7 @@ def generate_launch_description():
         PythonLaunchDescriptionSource(
             os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')),
         launch_arguments={
-            'gz_args': '-r first_scene_world_v2_ap.sdf'
+            'gz_args': '-r first_scene_world_v1_ap.sdf'
         }.items(),
     )
 
